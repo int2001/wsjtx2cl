80e20dec0bbda95b71eecd1cd8e34e2b80bf2a05
Joerg <joerg@dj7nt.de>
2024-01-19 10:13:52 +0100

    Added Readme

--- forge.config.js
+++ forge.config.js
@@ -16,10 +16,6 @@ module.exports = {
       name: '@electron-forge/maker-deb',
       config: {},
     },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
   ],
   plugins: [
     {
