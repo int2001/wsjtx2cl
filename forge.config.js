b2a943fe8ce0506f34a8f3460d2b6ca05e209e3c
Joerg <joerg@dj7nt.de>
2024-04-20 13:38:58 +0200

    DMG-Release

--- forge.config.js
+++ forge.config.js
@@ -1,38 +1,42 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-publishers: [
-    {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'int2001',
-          name: 'wsjtx2cl'
-        },
-        prerelease: true
-      }
-    }
-  ],
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: { icon: "./favicon.ico", maintainer: 'DJ7NT', loadingGif: "loading.gif", name: "SW2CL_DJ7NT" },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+	packagerConfig: {
+		// set config executableName
+		executableName: "wlgate",
+		icon: './favicon.ico',
+		asar: true,
+	},
+	publishers: [
+		{
+			name: '@electron-forge/publisher-github',
+			config: {
+				repository: {
+					owner: 'int2001',
+					name: 'wsjtx2cl'
+				},
+				prerelease: true
+			}
+		}
+	],
+	rebuildConfig: {},
+	makers: [
+		{
+			name: '@electron-forge/maker-squirrel',
+			config: { icon: "./favicon.ico", maintainer: 'DJ7NT', loadingGif: "loading.gif", name: "SW2CL_DJ7NT" },
+		},
+		{
+			name: '@electron-forge/maker-dmg',
+			platforms: ['darwin'],
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: { "bin":"wlgate" },
+			arch: ['x86']
+		},
+	],
+	plugins: [
+		{
+			name: '@electron-forge/plugin-auto-unpack-natives',
+			config: {},
+		},
+	],
 };
